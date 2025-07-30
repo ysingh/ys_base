a787b8a21c3cb21cbbd2aec207bf31cb07f97201
ysingh <ysingh0923@gmail.com>
2025-07-30 18:20:30 -0400

    Added ray struct definitions

--- src/ys_geom.h
+++ src/ys_geom.h
@@ -1,5 +1,19 @@
 #ifndef YS_GEOM_H
 #define YS_GEOM_H
 
+typedef struct ray2 {
+    point2 origin;
+    vec2 dir;
+} ray2;
+
+typedef struct ray3 {
+    point3 origin;
+    vec3 dir;
+} ray3;
+
+typedef struct ray4 {
+    point4 origin;
+    vec4 dir;
+} ray4;
 
 #endif
